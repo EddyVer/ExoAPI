9f71df5ae320712f5f39eded0b1b86746ccfb3ad
Eddy Vervoort <vervoorteddy@outlook.com>
2022-11-03 14:24:00 +0100

    oublie de save

--- ExoAPI/Controllers/EntrepotController.cs
+++ ExoAPI/Controllers/EntrepotController.cs
@@ -1,6 +1,14 @@
-﻿namespace ExoAPI.Controllers
+﻿using AutoMapper;
+using ExoAPI.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExoAPI.Controllers
 {
+    [ApiController]
+    [Route("[Controller]")]
     public class EntrepotController
     {
+        private readonly IMapper _mapper;
+        private readonly BusinessContext _businessContext;
     }
 }
